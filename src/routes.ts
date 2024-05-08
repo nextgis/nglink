a238dd99f68a725d03b5b6c4593f4853bd1c6816
floatline <poltewski@gmail.com>
2024-05-08 17:51:21 +0200

    feat(server): add generate image route, change routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,12 @@ import cors from 'cors';
 import { Router } from 'express';
 import { createLink } from './controllers/createLink';
 import detectFormat from './controllers/detectFormat';
+import { generateImage } from './controllers/generateImage';
 
 const router = Router();
 
 router.get('/d', detectFormat);
 router.post('/create-link', cors(), createLink);
+router.get('/', generateImage);
 
 export default router;
